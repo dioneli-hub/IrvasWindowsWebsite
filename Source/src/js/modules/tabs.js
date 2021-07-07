9bb046e8b549e4f08254847ed1b0df871e1324f1
Dianka Levchenko <dianka_levchenko@inbox.ru>
2021-07-07 16:06:29 +0300

    Windows tabs work.

---
+++ Source/src/js/modules/tabs.js
@@ -0,0 +1,37 @@
+const tabs = (headerSelector, tabSelector, contentSelector, activeClass) => {
+    const header = document.querySelector(headerSelector),
+        tabs = document.querySelectorAll(tabSelector),
+        content = document.querySelectorAll(contentSelector);
+
+    function hideTabContent(){
+        content.forEach(item => {
+            item.style.display = "none";
+        })
+
+        tabs.forEach(tab => {
+            tab.classList.remove(activeClass);
+        })
+    }
+
+    function  showTabContent(i = 0){
+        content[i].style.display = "block";
+        tabs[i].classList.add(activeClass);
+    }
+
+
+    header.addEventListener('click', (e)=>{
+        const target = e.target;
+        if (target &&
+            (target.classList.contains(tabSelector.replace(/\./,""))
+        || target.parentNode.classList.contains(tabSelector.replace(/\./,"")))){
+            tabs.forEach((tab, i) => {
+                if (target == tab || target.parentNode == tab){
+                    hideTabContent();
+                    showTabContent(i);
+                }
+            });
+        }
+    });
+};
+
+export default tabs;
