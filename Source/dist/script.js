c7796dc62e803b133c23aa7dbb6a68dc1b69dfb3
Dianka Levchenko <dianka_levchenko@inbox.ru>
2021-07-08 18:28:37 +0300

    Connected the calc form's steps.

--- Source/dist/script.js
+++ Source/dist/script.js
@@ -18027,28 +18027,39 @@ __webpack_require__.r(__webpack_exports__);
 
 var modals = function modals() {
   function bindModal(triggerSelector, modalSelector, closeSelector) {
+    var closeClickOverlay = arguments.length > 3 && arguments[3] !== undefined ? arguments[3] : true;
     var triggers = document.querySelectorAll(triggerSelector),
         modal = document.querySelector(modalSelector),
         close = document.querySelector(closeSelector),
-        scroll = Object(_calcScroll__WEBPACK_IMPORTED_MODULE_1__["default"])();
+        scroll = Object(_calcScroll__WEBPACK_IMPORTED_MODULE_1__["default"])(),
+        windows = document.querySelectorAll("[data-modal]");
     triggers.forEach(function (trigger) {
       trigger.addEventListener('click', function (e) {
         if (e.target) {
           e.preventDefault();
         }
 
+        windows.forEach(function (item) {
+          item.style.display = 'none';
+        });
         modal.style.display = "block";
         document.body.style.overflow = "hidden";
         document.body.style.marginRight = "".concat(scroll, "px"); // document.body.classList.add("modal-open");
       });
     });
     close.addEventListener("click", function () {
+      windows.forEach(function (item) {
+        item.style.display = 'none';
+      });
       modal.style.display = "none";
       document.body.style.overflow = "";
       document.body.style.marginRight = "0px"; // document.body.classList.remove("modal-open");
     });
     modal.addEventListener("click", function (e) {
-      if (e.target === modal) {
+      if (e.target === modal && closeClickOverlay) {
+        windows.forEach(function (item) {
+          item.style.display = 'none';
+        });
         modal.style.display = "none";
         document.body.style.overflow = "";
         document.body.style.marginRight = "0px"; // document.body.classList.remove("modal-open");
@@ -18064,6 +18075,8 @@ var modals = function modals() {
   }
 
   bindModal(".popup_calc_btn", ".popup_calc", ".popup_calc_close");
+  bindModal(".popup_calc_button", ".popup_calc_profile", ".popup_calc_profile_close", false);
+  bindModal(".popup_calc_profile_button", ".popup_calc_end", ".popup_calc_end_close", false);
   bindModal(".popup_engineer_btn", ".popup_engineer", ".popup_engineer .popup_close");
   bindModal(".phone_link", ".popup", ".popup .popup_close");
   showModalByTime(".popup", 60000);
--- Source/src/js/modules/modals.js
+++ Source/src/js/modules/modals.js
@@ -1,17 +1,23 @@
 import calcScroll from "./calcScroll";
 
 const modals = () => {
-    function bindModal(triggerSelector, modalSelector, closeSelector) {
+    function bindModal(triggerSelector, modalSelector, closeSelector, closeClickOverlay = true) {
         const triggers = document.querySelectorAll(triggerSelector),
             modal = document.querySelector(modalSelector),
             close = document.querySelector(closeSelector),
-            scroll = calcScroll();
+            scroll = calcScroll(),
+            windows = document.querySelectorAll("[data-modal]");
 
         triggers.forEach(trigger => {
             trigger.addEventListener('click', (e) => {
                 if (e.target) {
                     e.preventDefault();
                 }
+
+                windows.forEach(item => {
+                    item.style.display = 'none';
+                });
+
                 modal.style.display = "block";
                 document.body.style.overflow = "hidden";
                 document.body.style.marginRight = `${scroll}px`
@@ -20,6 +26,9 @@ const modals = () => {
         });
 
         close.addEventListener("click", () => {
+            windows.forEach(item => {
+                item.style.display = 'none';
+            });
             modal.style.display = "none";
             document.body.style.overflow = "";
             document.body.style.marginRight = `0px`;
@@ -27,7 +36,10 @@ const modals = () => {
         });
 
         modal.addEventListener("click", (e) => {
-            if (e.target === modal) {
+            if (e.target === modal && closeClickOverlay) {
+                windows.forEach(item => {
+                    item.style.display = 'none';
+                });
                 modal.style.display = "none";
                 document.body.style.overflow = "";
                 document.body.style.marginRight = `0px`;
@@ -44,6 +56,8 @@ const modals = () => {
     }
 
     bindModal(".popup_calc_btn", ".popup_calc", ".popup_calc_close");
+    bindModal(".popup_calc_button", ".popup_calc_profile", ".popup_calc_profile_close", false);
+    bindModal(".popup_calc_profile_button", ".popup_calc_end", ".popup_calc_end_close", false);
     bindModal(".popup_engineer_btn", ".popup_engineer", ".popup_engineer .popup_close");
     bindModal(".phone_link", ".popup", ".popup .popup_close");
     showModalByTime(".popup", 60000);
